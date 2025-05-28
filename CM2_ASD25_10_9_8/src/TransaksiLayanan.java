163f42b1717e3f0ae9864b01ee15c3f7b58b2d03
fifanuurunhalizah09 <eljeaje@gmail.com>
2025-05-28 11:16:32 +0700

    update

---
+++ CM2_ASD25_10_9_8/src/TransaksiLayanan.java
@@ -0,0 +1,18 @@
+class TransaksiLayanan {
+
+    Pasien pasien;
+    Dokter dokter;
+    int durasiLayanan;
+    double biaya = 50.000;
+
+    public TransaksiLayanan(Pasien pasien, Dokter dokter, int durasiLayanan) {
+        this.pasien = pasien;
+        this.dokter = dokter;
+        this.durasiLayanan = durasiLayanan;
+    }
+
+    public double hitungBiaya() {
+        double totalBayar = durasiLayanan * biaya;
+        return totalBayar;
+    }
+}
